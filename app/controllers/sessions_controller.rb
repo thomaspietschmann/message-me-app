5b9f30625c6e49c1f1329c03154a403bc3efbfdc
Thomas Pietschmann <thomas@pietschi.com>
2021-01-22 23:12:57 +0100

    changes database to pg for heroku

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
   def new
+
   end
 
   def create
@@ -22,5 +25,12 @@ end
     redirect_to login_path
   end
 
+  private
 
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
 end
