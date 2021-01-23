a12ae5cab872ef9dcf79bc6c7c5b30d52a2bc321
Thomas Pietschmann <thomas@pietschi.com>
2021-01-23 23:04:03 +0100

    generate action cable channel for chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post "login", to: 'sessions#create'
   post "message", to: "messages#create"
   delete "logout", to: "sessions#destroy"
+
+  mount ActionCable.server, at: "/cable"
 end
